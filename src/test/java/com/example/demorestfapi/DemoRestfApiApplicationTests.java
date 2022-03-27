eec4561ef97bcfd3b98ccc6d18803d0993650fd7
diqksrk <diqksrk123@naver.com>
2022-03-27 22:13:40 +0900

    project creation

---
+++ src/test/java/com/example/demorestfapi/DemoRestfApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demorestfapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestfApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
