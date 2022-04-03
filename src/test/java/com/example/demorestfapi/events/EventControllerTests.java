5182b377d9bf251f9e9df31382a5e5726c1c2264
diqksrk <diqksrk123@naver.com>
2022-04-03 15:46:52 +0900

    Validate wrong data

--- src/test/java/com/example/demorestfapi/events/EventControllerTests.java
+++ src/test/java/com/example/demorestfapi/events/EventControllerTests.java
@@ -92,4 +92,39 @@ public class EventControllerTests {
         ;
     }
 
+    @Test
+    public void createEvent_Bad_Request_Empty_Input() throws Exception {
+        EventDto eventDto = EventDto.builder().build();
+
+        this.mockMvc.perform(post("/api/events")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(this.objectMapper.writeValueAsString(eventDto))
+        )
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void createEvent_Wrong_Input() throws Exception {
+        EventDto event = EventDto.builder()
+                .name("Spring")
+                .description("sdadasd")
+                .beginEnrollmentDateTime(LocalDateTime.of(2018, 11,26, 11, 21))
+                .closeEnrollmentDateTime(LocalDateTime.of(2018, 11, 25, 11, 21))
+                .beginEventDateTime(LocalDateTime.of(2018, 11, 25, 11, 21))
+                .endEventDateTime(LocalDateTime.of(2018, 11, 26 ,11, 21))
+                .basePrice(10000)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타텁 팩토리")
+                .build();
+
+        mockMvc.perform(post("/api/events")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaTypes.HAL_JSON)
+                    .content(objectMapper.writeValueAsString(event))
+                )
+                .andExpect(status().isBadRequest())
+        ;
+    }
 }
